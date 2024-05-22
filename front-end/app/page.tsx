64694fa88b513c11deefe31e60cd39c579ad664f
tungnx-0469 <nguyen.xuan.tung-d@sun-asterisk.com>
2024-05-23 02:09:47 +0700

    fix navbar layout

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,5 +1,8 @@
+'use client'
 import Image from 'next/image';
-
+import { useRouter } from 'next/navigation';
 export default function Home() {
+  const router = useRouter()
+  router.push('/home')
   return <div>page</div>;
 }
--- front-end/components/Navbar/NavBar.tsx
+++ front-end/components/Navbar/NavBar.tsx
@@ -27,8 +27,8 @@ useEffect(() => {console.log(searching)
 
   return (
     <>
-      <div className="w-full  fixed top-0 z-50 bg-white">
-        <div className="h-full w-full shadow-md grid grid-cols-6 items-center justify-center align-baseline">
+      <div className="w-screen fixed top-0 z-50 bg-white">
+        <div className="h-full w-full shadow-md flex justify-between items-center align-baseline px-10">
           <div className="pl-10 h-full flex gap-1 items-center select-none col-span-2">
             <img src={Logo.src} alt="" className=" block h-20" />
 
@@ -60,51 +60,47 @@ useEffect(() => {console.log(searching)
             </Button>
           </div>
 
-          <div className="flex justify-center items-center">
-            <Button 
-            className="min-w-[150px]"
-            onClick={() => router.push('/home/collection/create')} >Create collection</Button>
-          </div>
-          <div className="flex justify-center items-center">
-            <Button 
-            className="min-w-[150px]"
-            onClick={() => router.push('/home/class/create-class')} >Create class</Button>
-          </div>
-          <div className="flex justify-center items-center">
-            <Button 
-            className="min-w-[150px]"
-            onClick={() => router.push('/home/class/create-class')} >Create class</Button>
-          </div>
 
-          <div className="flex justify-start items-center">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  variant="outline"
-                  color="blue-gray"
-                  className=" rounded-full h-10 w-10 bg-blue-100/20 py-0 pr-0 pl-0 ">
-                  <User className=" h-10 w-10 text-blue-300 fill-blue-300  rounded-full p-1.5" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuGroup>
-                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>
-                    <User className="mr-2 h-4 w-4" />
-                    <span onClick={() => router.push('/home/profile/profile')}>Profile</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <Settings className="mr-2 h-4 w-4" />
-                    <span>Settings</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <LogOut className="mr-2 h-4 w-4" />
-                    <span><Link href="\auth\login">Log out</Link></span>
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
+          <div className='flex gap-10'>
+           <div className='flex gap-6'>
+                <Button 
+                className="w-auto"
+                onClick={() => router.push('/home/collection/create')} >Create collection</Button>
+                <Button 
+                className="w-auto"
+                onClick={() => router.push('/home/class/create-class')} >Create class</Button>
+            </div>
+              
+            <div className="flex justify-start items-center">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="outline"
+                    color="blue-gray"
+                    className=" rounded-full h-10 w-10 bg-blue-100/20 py-0 pr-0 pl-0 ">
+                    <User className=" h-10 w-10 text-blue-300 fill-blue-300  rounded-full p-1.5" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuGroup>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                      <User className="mr-2 h-4 w-4" />
+                      <span onClick={() => router.push('/home/profile/profile')}>Profile</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Settings className="mr-2 h-4 w-4" />
+                      <span>Settings</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <LogOut className="mr-2 h-4 w-4" />
+                      <span><Link href="\auth\login">Log out</Link></span>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
           </div>
         </div>
       </div>
