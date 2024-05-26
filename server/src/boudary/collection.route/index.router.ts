f784615d2fc3a072b003af29fe48c920c0247aa1
Pham-Duc-Luu <pl.podcast.08@gmail.com>
2024-05-26 23:18:03 +0700

    fixing

--- server/src/boudary/collection.route/index.router.ts
+++ server/src/boudary/collection.route/index.router.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Router } from "express";
 import authController from "../../controller/auth.controller";
 import collectionController from "../../controller/collection.controller";
@@ -11,24 +10,6 @@ collectionRouter.post(
   collectionController.createCollection
 );
 
-collectionRouter.post("/add-flash-card", collectionController.addFlashCard);
 // collectionRouter.post('/forgot-password', authController.sign_up);
 
 export default collectionRouter;
-=======
-import { Router } from 'express';
-import authController from '../../controller/auth.controller';
-import collectionController from '../../controller/collection.controller';
-
-const collectionRouter = Router();
-
-// TODO: create a sign-up route
-collectionRouter.post(
-  '/create-collection',
-  collectionController.createCollection
-);
-
-// collectionRouter.post('/forgot-password', authController.sign_up);
-
-export default collectionRouter;
->>>>>>> refs/remotes/origin/main
