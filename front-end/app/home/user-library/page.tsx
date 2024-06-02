8f640e8ce752c558102d2d1425af9137d19e564c
eribreak <transyhieu987@gmail.com>
2024-06-02 20:56:56 +0700

    add library

--- front-end/app/home/user-library/page.tsx
+++ front-end/app/home/user-library/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { FakeCollectionData } from '@/API/FakeData';
+import { FakeCollectionData, FakeClassData } from '@/API/FakeData';
 import Share from '@/components/Svg/Share'
 import Plus from '@/components/Svg/Plus';
 import Three_dot from '@/components/Svg/Three_dot';
 import CollectionCard from '@/components/Collection/CollectionCard';
+import ClassCard from '@/components/Class/ClassCard';
 // * list of collections user have
 // * list of class user have
 const page = () => {
@@ -15,7 +16,7 @@ const page = () => {
             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0,0,256,256" fontStyle="fill:#6B7280">
                 <g fill="#6B7280" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" fontStyle="mix-blend-mode: normal"><g transform="scale(10.66667,10.66667)"><path d="M4,4c-1.09425,0 -2,0.90575 -2,2v12c0,1.09426 0.90575,2 2,2h16c1.09426,0 2,-0.90574 2,-2v-10c0,-1.09425 -0.90574,-2 -2,-2h-8l-2,-2zM4,6h5.17188l2,2h8.82813v10h-16z"></path></g></g>
             </svg>
-              My collections
+              My library
             </h1>
             
             <div className='flex gap-3'>
@@ -41,8 +42,16 @@ const page = () => {
               </div>
           </div>
         </div>
-        <div className='flex gap-5 flex-wrap'>
+
+        <div className='font-bold text-4xl my-10'>Collections</div>
+        <div className='flex gap-16 flex-wrap'>
                   {FakeCollectionData.map((collection, index) => <CollectionCard key={index} collection={collection}/>)}
+        </div>
+        <div className='font-bold text-4xl my-10'>Classes</div>
+        <div className="flex gap-10 mt-10">
+          {FakeClassData.map((c, i) => (
+            <ClassCard key={i} classes={c} />
+          ))}
         </div>
 
     </div>
