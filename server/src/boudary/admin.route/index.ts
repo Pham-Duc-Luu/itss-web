167860550c91ebce30150ed13faa1cd920813a18
eribreak <transyhieu987@gmail.com>
2024-06-12 00:17:49 +0700

    abc

---
+++ server/src/boudary/admin.route/index.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import adminController from "../../controller/admin.controller";
+import { list } from "@sequelize/core/lib/expression-builders/list";
+import { auth } from "google-auth-library";
+
+const adminRouter = Router();
+
+
+/**
+ * * login with email and password
+ */
+adminRouter.post("/admin", adminController.login);
+
+/**
+ * * View user list
+ */
+// xem ds ng dung
+adminRouter.post("/view-users", adminController.viewUserList);
+
+// xem ds class  
+adminRouter.post("/view-class-list", adminController.viewClassList);
+
+// xoa user 
+adminRouter.post("/remove-user", adminController.removeUser);
+// sua tt user 
+adminRouter.patch("/update-user", adminController.updateUser);
+// xoa class
+adminRouter.post("/remove-class", adminController.removeClass);
+// sua class
+adminRouter.patch("/update-class", adminController.updateClass);
+
+export default adminRouter;
---
+++ server/src/controller/admin.controller.ts
@@ -0,0 +1,52 @@
+import { Response, Request } from "express";
+import {
+  BadRequest,
+  HttpErrorResponse,
+  InvalidParameter,
+  MissingParameter,
+} from "../lib/http.reponse";
+import Logger from "../lib/logger";
+import { validateService } from "../service/validate.service";
+import { prisma } from "../database/postgresql/connect.postgresql";
+
+class AdminController {
+  async login() {
+    return 'Admin';
+  }
+  async viewUserList(
+    req: Request,
+    res: Response
+  ) {
+    try {
+        const users = await prisma.user.findMany();
+        return res.status(200).json({ data: users });
+        
+    }
+    catch (error: any) {
+       console.log(error.stack);
+      const err = new HttpErrorResponse(error.message, error.statusCode);
+      return res.status(err.statusCode).json({ message: err.message });
+    };
+  }
+  async viewClassList() {    
+    return 'Admin';
+  }
+  async addUser() {
+    return 'Admin';
+  }
+  async removeUser() {
+    return 'Admin';
+  }
+  async updateUser() {  
+    return 'Admin';
+  }
+  async removeClass() {
+    return 'Admin';
+  }
+  async updateClass() { 
+    return 'Admin';
+  }
+}
+
+const adminController = new AdminController();
+export default adminController;
