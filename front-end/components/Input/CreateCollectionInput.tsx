99aa76f28a635ddd05acaa751e0fc4a5ff0a9bd6
tungnx-0469 <nguyen.xuan.tung-d@sun-asterisk.com>
2024-06-13 00:17:18 +0700

    abc

--- front-end/components/Input/CreateCollectionInput.tsx
+++ front-end/components/Input/CreateCollectionInput.tsx
@@ -11,7 +11,12 @@ function CreateCollectionInput({
   postContent,
   ...inputParams
 }: any) {
-  return <></>;
+  return  
+  <div className='w-full'>
+    <input type="text" name={name} id="" placeholder={placeholder} className='bg-transparent w-full border-b-2 border-gray-600 focus:outline-0 mb-2 placeholder:text-slate-300 py-3 focus:border-b-4 focus:border-sky-700' {...inputParams}/>
+    <input type="text" name={name} id="" placeholder={placeholder} className='bg-transparent w-full border-b-2 border-gray-600 focus:outline-0 mb-2 placeholder:text-slate-300 py-3 focus:border-b-4 focus:border-cyan-500' {...inputParams}/>
+    <label htmlFor={name} className='block text-xs text-gray-400 font-medium uppercase tracking-wider'>{label}</label>
+</div>;
 }
 
 export default CreateCollectionInput;
