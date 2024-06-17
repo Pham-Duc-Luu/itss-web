f9bfa368600e2b2392b7d6657e63b467106445cd
tungnx-0469 <nguyen.xuan.tung-d@sun-asterisk.com>
2024-06-17 12:11:14 +0700

    ađd test

--- front-end/__tests__/sum.test.ts
+++ front-end/__tests__/sum.test.ts
@@ -1,13 +1,20 @@
 import { describe, expect, test } from "@jest/globals";
-const sum = (a: number, b: number) => {
-  return a + b;
-};
 
 import collectionApi from "../lib/CollectionApi";
 import { number } from "zod";
-describe("sum module", () => {
-  test("view collection", () => {
+import adminApi from "@/lib/AdminApi";
+
+describe("Test colelction api", () => {
+  test("Test collection aii", () => {
     return collectionApi.viewCollection().then((res) => {
+      expect(res.data.data[0]).toHaveProperty("id");
+    });
+  });
+});
+
+describe("Test admin api", () => {
+  test("view admin api", () => {
+    return adminApi.getClassData().then((res) => {
       expect(res.data.data[0]).toHaveProperty("id");
     });
   });
